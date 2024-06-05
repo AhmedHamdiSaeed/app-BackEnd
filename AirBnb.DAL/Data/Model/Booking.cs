8dde687df7438ebb7b4aa6c97f64f875c30694d1
Ahmed Hamdi Saeed Ibrahim <135139481+AhmedHamdiSaeed@users.noreply.github.com>
2024-06-06 01:18:21 +0300

    edit

--- AirBnb.DAL/Data/Model/Booking.cs
+++ AirBnb.DAL/Data/Model/Booking.cs
@@ -16,17 +16,19 @@ namespace AirBnb.DAL.Data.Model
 	public class Booking:BaseEntity<int>
 	{
 
-		[ForeignKey(nameof(Property))]
-		public int PropertyId { get; set; }
-		public Property Property { get; set; }
+        [ForeignKey(nameof(Property))]
+        public int PropertyId { get; set; }
+        public Property Property { get; set; } = null!;
 
-		[ForeignKey(nameof(User))]
-		public string UserId { get; set; } = string.Empty;
-		public AppUser User {  get; set; }
-		public DateTime CheckInDate { get; set; }
-		public DateTime CheckOutDate { get; set;}
-		public int TotalPrice { get; set; }
-		public Status BookingStatus { get; set; }
-		
-	}
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = string.Empty;
+        public AppUser User { get; set; } = null!;
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public int TotalPrice { get; set; }
+        public Status BookingStatus { get; set; }
+        public IEnumerable<AppointmentsAvailable> AppointmentsAvailable { get; set; } = new HashSet<AppointmentsAvailable>();
+        public List<Review> Reviews { get; set; } = new List<Review>();
+
+    }
 }
